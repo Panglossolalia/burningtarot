bb0130a6625c9efed4743ae38fdb45957d486eb9
panglossolalia <cglucas@gmail.com>
2022-10-25 00:44:13 +0100

    commented our minor arcana

--- cardpicker.js
+++ cardpicker.js
@@ -155,7 +155,7 @@ const tarot = [
 		"image": "img/11 - Justice.jpeg",
 		"description": "As this final message of the Major Trumps is unchanged--and indeed unchangeable--in respect of its design, it has been partly described already regarding its deeper sense. It represents also the perfection and end of the Cosmos, the secret which is within it, the rapture of the universe when it understands itself in God. It is further the state of the soul in the consciousness of Divine Vision, reflected from the self-knowing spirit. But these meanings are without prejudice to that which I have said concerning it on the material side. It has more than one message on the macrocosmic side and is, for example, the state of the restored world when the law of manifestation shall have been carried to the highest degree of natural perfection. But it is perhaps more especially a story of the past, referring to that day when all was declared to be good, when the morning stars sang together and all the Sons of God shouted for joy. One of the worst explanations concerning it is that the figure symbolizes the Magus when he has reached the highest degree of initiation; another account says that it represents the absolute, which is ridiculous. The figure has been said to stand for Truth, which is, however, more properly allocated to the seventeenth card. Lastly, it has been called the Crown of the Magi.",
 		"interpretation": "Assured success, recompense, voyage, route, emigration, flight, change of place. Reversed: Inertia, fixity, stagnation, permanence."
-	},
+	} /*,
 	{
 		"name": "King of Wands",
 		"suite": "Wands",
@@ -547,7 +547,7 @@ const tarot = [
 		"image": "img/11 - Justice.jpeg",
 		"description": "A hand--issuing, as usual, from a cloud--holds up a pentacle.",
 		"interpretation": "Perfect contentment, felicity, ecstasy; also speedy intelligence; gold. Reversed: The evil side of wealth, bad intelligence; also great riches. In any case it shews prosperity, comfortable material conditions, but whether these are of advantage to the possessor will depend on whether the card is reversed or not."
-	}
+	}*/
 ]
 
 const phases = ["The starting point",
